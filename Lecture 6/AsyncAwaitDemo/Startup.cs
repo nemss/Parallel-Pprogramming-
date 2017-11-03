af6dbd4f2bed07e6bfd8e05fb99ae2e5b984eddf
Valentin <valentin_github@abv.bg>
2017-11-03 11:40:45 +0200

    Add Lecture 6

---
+++ Lecture 6/AsyncAwaitDemo/Startup.cs
@@ -0,0 +1,40 @@
+﻿namespace AsyncAwaitDemo
+{
+    using System;
+    using System.Data.SqlClient;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class Startup
+    {
+        private const string ConnectionString = "Server=.;Database=ShopDb;Integrated Security=True;";
+        private const string DummyCommand = "WAITFOR DELAY '00:00:01:000';";
+
+        public static void Main()
+        {
+            Console.WriteLine($"Main thread id {Thread.CurrentThread.ManagedThreadId}.");
+
+            var t = CallDatabase();
+            t.Wait();
+
+            Console.WriteLine("Press ENTER to quit.");
+            Console.ReadLine();
+        }
+
+        private static async Task CallDatabase()
+        {
+            var conn = new SqlConnection(ConnectionString);
+            conn.Open();
+            var cmd = new SqlCommand(DummyCommand, conn);
+
+            Console.WriteLine($"Thread id {Thread.CurrentThread.ManagedThreadId}.");
+
+            Console.WriteLine("Begin async execution...");
+
+            await cmd.ExecuteNonQueryAsync();
+
+            Console.WriteLine("Async execution completed.");
+            Console.WriteLine($"Thread id {Thread.CurrentThread.ManagedThreadId}.");
+        }
+    }
+}
---
+++ Lecture 6/AsyncResultPattern/Startup.cs
@@ -0,0 +1,42 @@
+﻿namespace AsyncResultPattern
+{
+    using System;
+    using System.Data.SqlClient;
+    using System.Threading;
+
+    public class Startup
+    {
+        private const string ConnectionString = "Server=.;Database=ShopDb;Integrated Security=True;";
+        private const string DummyCommand = "WAITFOR DELAY '00:00:01:000';";
+
+        public static void Main()
+        {
+            var conn = new SqlConnection(ConnectionString);
+            conn.Open();
+            using (conn)
+            {
+                Console.WriteLine($"Main thread id: {Thread.CurrentThread.ManagedThreadId}.");
+                var cmd = new SqlCommand(DummyCommand, conn);
+                Console.WriteLine("Execute db cmd synchronously");
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Synchronous execution completed.");
+
+                Console.WriteLine();
+
+                Console.WriteLine("Execute db cmd asynchronously.");
+                cmd.BeginExecuteNonQuery(ExecQueryCallBack, null);
+                Console.WriteLine("I am free to continue");
+
+
+                Console.WriteLine("Press Enter to quit.");
+                Console.ReadLine();
+            }
+        }
+
+        static void ExecQueryCallBack(IAsyncResult result)
+        {
+            Console.WriteLine("Asynchronous execution completed.");
+            Console.WriteLine($"Thread id: {Thread.CurrentThread.ManagedThreadId}.");
+        }
+    }
+}
